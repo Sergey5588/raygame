69b3369cb542716174a66a8c24673eef90cb4367
Sergey5588 <sergey271109@gmail.com>
2025-07-12 12:10:27 +0300

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [ 
+	gcc
+	libGL
+	xorg.libX11
+  libpng
+	xorg.libX11.dev
+  raylib
+    ];
+    LD_LIBRARY_PATH="/home/sergey/projects/c/raylib/lib/:$LD_LIBRARY_PATH";
+
+}
